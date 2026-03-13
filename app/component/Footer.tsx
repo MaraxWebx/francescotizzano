eaf1466c2d36e9e084a5a232c54cfca170772c39
MaraxWebx <antoniomarettax@gmail.com>
2026-03-13 21:09:17 +0100

    New page bio 123

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -1,51 +1,61 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Footer() {
+  const pathname = usePathname(); // legge il path corrente
+
+  const isProfile = pathname === "/profile"; // verifica se siamo nella pagina gallery
+
   return (
     <div className="fixed bottom-0 left-0 w-full flex items-center justify-between pb-6 px-6 z-50">
       {/* SINISTRA — PROFILE */}
-      <Link
-        href="/profile"
-        className="text-white text-md tracking-wide relative transition-all duration-300 hover:scale-110 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[1px] after:bg-white hover:after:w-full after:transition-all after:duration-300"
-      >
-        Profile
-      </Link>
+      {!isProfile && (
+        <>
+          <Link
+            href="/profile"
+            className="text-white text-md tracking-wide relative transition-all duration-300 hover:scale-110 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[1px] after:bg-white hover:after:w-full after:transition-all after:duration-300"
+          >
+            Profile
+          </Link>
+          <>
+            {/* DESTRA — CONTATTI */}
+            <div className="flex flex-col md:flex-row gap-2 md:gap-4 items-end">
+              <a
+                href="https://www.instagram.com/francescotizzano/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white text-xs opacity-80 hover:opacity-100 transition-opacity"
+              >
+                @francescotizzano
+              </a>
+              <a
+                href="mailto:francescotizzano89@gmail.com"
+                className="text-white text-xs opacity-80 hover:opacity-100 transition-opacity"
+              >
+                francescotizzano89@gmail.com
+              </a>
+              <a
+                href="tel:+393484713563"
+                className="text-white text-xs opacity-80 hover:opacity-100 transition-opacity"
+              >
+                +39 348 471 3563
+              </a>
 
-      {/* DESTRA — CONTATTI */}
-      <div className="flex flex-col md:flex-row gap-2 md:gap-4 items-end">
-        <a
-          href="https://www.instagram.com/francescotizzano/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-white text-xs opacity-80 hover:opacity-100 transition-opacity"
-        >
-          @francescotizzano
-        </a>
-        <a
-          href="mailto:francescotizzano89@gmail.com"
-          className="text-white text-xs opacity-80 hover:opacity-100 transition-opacity"
-        >
-          francescotizzano89@gmail.com
-        </a>
-        <a
-          href="tel:+393484713563"
-          className="text-white text-xs opacity-80 hover:opacity-100 transition-opacity"
-        >
-          +39 348 471 3563
-        </a>
-
-        <a
-          href="https://www.instagram.com/marettax_/?hl=it"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-white text-xs opacity-80 hover:opacity-100 transition-opacity"
-          style={{ fontSize: 8 }}
-        >
-          ❤️☠️Marettax
-        </a>
-      </div>
+              <a
+                href="https://www.instagram.com/marettax_/?hl=it"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white text-xs opacity-80 hover:opacity-100 transition-opacity"
+                style={{ fontSize: 8 }}
+              >
+                ❤️☠️Marettax
+              </a>
+            </div>
+          </>
+        </>
+      )}
     </div>
   );
 }
